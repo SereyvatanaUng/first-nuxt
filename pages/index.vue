11c076d12370fce28a71d95d7c252c096f093515
SereyvatanaUng <sereyvatanau@gmail.com>
2024-01-22 15:56:25 +0700

    Starter files and add page

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h2>Home</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam fuga, quod
+      repudiandae totam nulla possimus obcaecati tempora blanditiis. Quaerat,
+      pariatur!
+    </p>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis inventore
+      placeat fugiat accusamus. Praesentium, id? A exercitationem perspiciatis
+      possimus minima?
+    </p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+p {
+  margin: 20px 0;
+}
+</style>
