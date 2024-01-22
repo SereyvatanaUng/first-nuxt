dfc614bb86d9b6d566e87e1ca37dd7e6cf9c3395
SereyvatanaUng <sereyvatanau@gmail.com>
2024-01-22 16:35:10 +0700

    Route parameters

---
+++ pages/products/[id].vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <p>Product details for {{ id }}</p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet sequi
+      dolor vel, dolorum nobis architecto aliquid voluptatum itaque rem officia?
+    </p>
+  </div>
+</template>
+
+<script setup>
+const { id } = useRoute().params;
+console.log(id);
+</script>
