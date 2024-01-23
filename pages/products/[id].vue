3cad8a247cf710a17d9b00b4a142e4a826672403
SereyvatanaUng <sereyvatanau@gmail.com>
2024-01-23 14:09:23 +0700

    Handle Error Page

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,16 +1,20 @@
 <template>
   <div>
-    <ProductDetails :product="product"/>
+    <ProductDetails :product="product" />
   </div>
 </template>
 
 <script setup>
 const { id } = useRoute().params;
 const uri = "https://fakestoreapi.com/products/" + id;
-console.log(uri);
 
 // fetch the product, use key to fetch the new data, otherwise it gonna show the first one.
 const { data: product } = await useFetch(uri, { key: id });
+
+if (!product.value) {
+  // fatal:true -> to force application to show error
+  throw createError({ statusCode: 404, statusMessage: "Product not found",fatal:true });
+}
 
 definePageMeta({ layout: "products" });
 </script>
